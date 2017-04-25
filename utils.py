224b0d1fcafa33a92b54e9202513b5d430ed299f
Romain Tavenard <romain.tavenard@univ-rennes2.fr>
2017-04-25 11:14:31 +0200

    JSON reader added

---
+++ utils.py
@@ -0,0 +1,21 @@
+import json
+import numpy
+
+__author__ = 'Romain Tavenard romain.tavenard[at]univ-rennes2.fr'
+
+
+def read_doc_json(fname):
+    doc_dict = json.load(open(fname, "rt"))
+    n_obs = len(doc_dict)
+    npy_doc = numpy.empty((n_obs, 2), dtype=numpy.int)
+    for i in range(n_obs):
+        npy_doc[i, :] = [doc_dict[i]["w"], doc_dict[i]["t"]]
+    n_words = numpy.max(npy_doc[:, 0]) + 1
+    n_t = numpy.max(npy_doc[:, 1]) + 1
+    return npy_doc, n_words, n_t
+
+
+if __name__ == "__main__":
+    doc, n_words, n_t = read_doc_json("sample_data/simple_doc.json")
+    print(doc)
+    print(n_words, n_t)

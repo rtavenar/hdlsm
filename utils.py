5921a2f11a9a28a3c554a1e5181e26f5a5580e18
rtavenar <romain.tavenard@univ-rennes2.fr>
2017-06-19 23:49:05 +0200

    Added AIS code + data

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import json
 import numpy
+import shapefile
 
 __author__ = 'Romain Tavenard romain.tavenard[at]univ-rennes2.fr'
 
@@ -15,7 +16,38 @@ def read_doc_from_json(fname):
     return npy_doc, n_words, n_t
 
 
+def load_trajectories(path):
+    path_pos = path + "_pos"
+    path_track = path + "_track"
+
+    sf_track = shapefile.Reader(path_track)
+    idx_ship_type = 4
+    assert sf_track.fields[idx_ship_type + 1][0] == "SHIP_TYPE"
+
+    sf_pos = shapefile.Reader(path_pos)
+    idx_timestamp = 14
+    assert sf_pos.fields[idx_timestamp + 1][0] == "UNIX_TIME"
+    positions = sf_pos.records()
+
+    docs = []
+    idx_start = 0
+    for idx_shape, shprec in enumerate(sf_track.shapeRecords()):
+        sz_traj = len(shprec.shape.points)
+        docs.append({"SHIP_TYPE": shprec.record[idx_ship_type],
+                     "TRAJECTORY": [{"lon": lon, "lat": lat, "t": pos[idx_timestamp]}
+                                    for (lon, lat), pos in zip(shprec.shape.points,
+                                                               positions[idx_start:idx_start+sz_traj])]})
+        idx_start += sz_traj
+    return docs
+
+
 if __name__ == "__main__":
     doc, n_words, n_t = read_doc_from_json("sample_data/simple_doc.json")
     print(doc)
     print(n_words, n_t)
+
+    print("---")
+
+    docs_ais = load_trajectories("sample_data/Ascension_032017/Ascension_2017_month3_AIS")
+    print(len(docs_ais))
+
